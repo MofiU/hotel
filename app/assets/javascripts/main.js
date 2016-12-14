fa08263b7fd14c311b4835e076cdc5d40ede71cc
bo.zeng <bo.zeng@laxino.com>
2016-12-14 14:32:52 +0800

    update nav ui

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,31 +1,72 @@
-$(function(){
-  $('#product-screenshots .owl-carousel-center').owlCarousel({
-    center: true,
-    items:2,
-    loop:false,
-    margin:10,
-    nav: true,
-    dots: true,
-    autoHeight: true,
-    smartSpeed: 500,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
-    responsive:{
-      0:{
-        items:1
-      },
-      600:{
-        items:4
-      },
-      1000:{
-        items:4
+;(function(){
+  // 'use strict';
+
+  var owlCrouselFeatureSlide = function() {
+    $('#product-screenshots .owl-carousel-center').owlCarousel({
+      center: true,
+      items:2,
+      loop:false,
+      margin:10,
+      nav: true,
+      dots: true,
+      autoHeight: true,
+      smartSpeed: 500,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:4
+        },
+        1000:{
+          items:4
+        }
       }
-    }
+    });
+    $('body').on('click', '#product-screenshots .owl-carousel-center .owl-item', function(){
+      var $this = $(this),
+      index = $this.index();
+      $('#product-screenshots .owl-carousel-center .owl-dots > div').eq(index).trigger('click');
+    });
+  };
+
+
+
+// Window Scroll
+  var windowScroll = function() {
+    $(window).scroll(function(event){
+      var navigation = $('#navigation'),
+      scrlTop = $(this).scrollTop();
+      if ( scrlTop > 500 && scrlTop <= 2000 ) {
+        navigation.addClass('navbar-fixed-top');
+      } else if ( scrlTop <= 500) {
+        if ( navigation.hasClass('navbar-fixed-top') ) {
+          navigation.addClass('navbar-fixed-top');
+          setTimeout(function(){
+            navigation.removeClass('navbar-fixed-top');
+          }, 100 );
+        }
+      }
+    });
+  };
+
+  var navActive = function(section) {
+
+    var $el = $('#navigation > ul');
+    $el.find('li').removeClass('active');
+    $el.each(function(){
+      $(this).find('a[data-nav-section="'+section+'"]').closest('li').addClass('active');
+    });
+
+  };
+
+
+  $(function(){
+    owlCrouselFeatureSlide();
+    windowScroll();
   });
-  $('body').on('click', '#product-screenshots .owl-carousel-center .owl-item', function(){
-    var $this = $(this),
-    index = $this.index();
-    $('#product-screenshots .owl-carousel-center .owl-dots > div').eq(index).trigger('click');
-  });
-})
+
+}())
